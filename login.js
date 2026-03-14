4565deeb99ec016769048d34b35e49f45e86d68a
hrw2013 <harrisonrobertwoods@gmail.com>
2026-03-13 22:31:32 -0400

    Create login.js

---
+++ login.js
@@ -0,0 +1,14 @@
+function login(){
+
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+auth.signInWithEmailAndPassword(email,password)
+.then(()=>{
+window.location.href="dashboard.html";
+})
+.catch((error)=>{
+alert(error.message);
+});
+
+}
